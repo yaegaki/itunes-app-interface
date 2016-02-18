c89f6ddb9bcb1df74ecf5c940c64a417c1826b40
yaegaki <yaegaki.fino@gmail.com>
2016-02-18 21:26:56 +0900

    add track.Play (Windows)

--- track_windows.go
+++ track_windows.go
@@ -1,9 +1,17 @@
 package itunes
 
-import "github.com/go-ole/go-ole"
+import (
+	"github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
+)
 
 type track struct {
 	handle *ole.IDispatch
 	Name   string
 	Artist string
+}
+
+func (t *track) Play() error {
+	_, err := oleutil.CallMethod(t.handle, "Play")
+	return err
 }
